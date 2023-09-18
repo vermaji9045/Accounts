a1758f4b8726ab3315fc361c5bb5d88788b3ae98
Sandeep <sk9366026@gmail.com>
2023-09-18 22:30:22 +0530

    Logs and Rest APIs

--- SchoolValleyProject/src/main/java/com/School/SchoolValleyProject/repository/ValleyClassRepository.java
+++
@@ -1,9 +0,0 @@
-package com.School.SchoolValleyProject.repository;
-
-import com.School.SchoolValleyProject.Model.ValleyClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValleyClassRepository extends JpaRepository<ValleyClass,Integer> {
-}
